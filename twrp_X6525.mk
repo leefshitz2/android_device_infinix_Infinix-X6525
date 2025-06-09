1f4ccc586b6b7df7d22c9070df3e3cb383cdb0e1
KSN <redawewrq12@gmail.com>
2025-06-09 17:36:56 +0300

    Update twrp_X6525.mk

--- twrp_X6525.mk
+++ twrp_X6525.mk
@@ -9,8 +9,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
+
+# Inherit some common pb stuff.
+$(call inherit-product-if-exists, vendor/pb/config/common.mk)
 
 # Inherit from Infinix-X6525 device
 $(call inherit-product, device/infinix/X6525/device.mk)
