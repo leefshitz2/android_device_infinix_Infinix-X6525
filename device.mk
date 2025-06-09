50d98c16880a7fe06e86ded500efdb742c6a8ade
KSN <redawewrq12@gmail.com>
2025-06-09 19:51:42 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,8 +15,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
     bootctrl.ums9230
